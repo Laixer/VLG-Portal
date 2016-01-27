b8376d225a28202b0fc266ed3bd78b2c98a02678
yorickdewid <yorick17@outlook.com>
2016-01-27 19:41:54 +0100

    Fix error in redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -24,22 +24,22 @@ class RedirectIfAuthenticated
             $request->session()->forget('sso');
         }
 
-        if ($request->get('token') && $request->get('endpoint') && $request->get('timestamp') && $request->get('auth')) {
-            if ($request->get('timestamp') > (time()-900) && $request->get('timestamp') < (time()+2) && $request->get('auth') == 'jwtgssauth') {
-                $app = Application::where('public_token', $request->get('token'))->where('domain', $request->get('endpoint'))->first();
-
-                $endpoint = $app->getEndpointUrl();
-
-                $audit = new Audit;
-                $audit->payload = 'SSO via ' . $app->domain;
-                $audit->user_id = Auth::id();
-                $audit->save();
-
-                return redirect($endpoint);
-            }
-        }
-
         if (Auth::guard($guard)->check()) {
+            if ($request->get('token') && $request->get('endpoint') && $request->get('timestamp') && $request->get('auth')) {
+                if ($request->get('timestamp') > (time()-900) && $request->get('timestamp') < (time()+2) && $request->get('auth') == 'jwtgssauth') {
+                    $app = Application::where('public_token', $request->get('token'))->where('domain', $request->get('endpoint'))->first();
+
+                    $endpoint = $app->getEndpointUrl();
+
+                    $audit = new Audit;
+                    $audit->payload = 'SSO via ' . $app->domain;
+                    $audit->user_id = Auth::id();
+                    $audit->save();
+
+                    return redirect($endpoint);
+                }
+            }
+
             return redirect('/');
         }
 
